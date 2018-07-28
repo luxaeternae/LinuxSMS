15df14b7a63aa2d685a591cd0c25299c26adb406
root <root@localhost.localdomain>
2018-07-27 23:39:18 -0400

    Setup process done

--- first-time-setup.py
+++ first-time-setup.py
@@ -1,7 +1,7 @@
 print "Welcome to the first time setup of your linux SMS application."
 print ""
 
-var1 = str(raw_input("What is your linux username?"))
+var1 = raw_input("What is your linux username?")
 var2 = raw_input("What is your gmail email account? e.g xxx@gmail.com")
 var3 = raw_input("What is your gmail password?")
 
@@ -12,7 +12,13 @@ print "This application is only compatible with gmail email servers. Please make
 print "Please have the gmail that you use for this be your default (inbox/0) inbox, or else inbox will not work and will redirect you to the wrong email."
 
 f = open("setupvars.txt", "w")
-f.write("pathuser = /home/%s/project/project-1A02-12094/contacts") % (var1)
-f.write("emaill = %s") % (var2)
-f.write("password = %s") %s (var3)
+f.write("pathuser = /home/%s/project/project-1A02-12094/contacts\n" % (var1))
+f.close()
+
+f = open("setupvars.txt", "a")
+f.write("emaill = %s\n" % (var2))
+f.close()
+
+f = open("setupvars.txt", "a")
+f.write("password = %s\n" % (var3))
 f.close()
--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,1 +1,7 @@
-ls
+echo "Running Set up"
+
+python first-time-setup.py
+
+sudo mv setupvars.txt vars.py
+
+mkdir contacts
