75a6491bc6cfce682bbbfabc3011732962b4d018
root <root@localhost.localdomain>
2018-07-28 09:41:00 -0400

    dependencies

--- first-time-setup.sh
+++ first-time-setup.sh
@@ -1,7 +1,10 @@
 echo "Running Set up"
 
+sudo apt-get install python
+sudo apt-get install python-gobject
+sudo apt-get install libnotify-bin
+sudo apt-get install libnotify-dev
+
 python first-time-setup.py
-
 sudo mv setupvars.txt vars.py
-
 mkdir contacts
